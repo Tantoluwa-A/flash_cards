a8861b0908c102e18987731192f814f9e9950439
Tantoluwa-A <tantoluwaadesina@gmail.com>
2022-07-15 13:59:55 -0700

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,37 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Card:");
-        System.out.println("purchase");
-        System.out.println("Definition:");
-        System.out.println("buy");
         Scanner scanner = new Scanner(System.in);
-        String a = scanner.nextLine();
-        String b = scanner.nextLine();
-        String c = scanner.nextLine();
-        if (b.equals(c)) {
-            System .out.println("Your answer is right!");
-        } else {
-            System.out.println("Your answer is wrong...");
+        ArrayList<String> terms = new ArrayList<>();
+        ArrayList<String> definitions = new ArrayList<>();
+
+        System.out.println("Input the number of cards: ");
+        int n = scanner.nextInt();
+        String o = scanner.nextLine();
+        String term = "";
+        String definition = "";
+
+        for (int i = 1; i < n + 1; i++) {
+            System.out.println("Card #" + i + ":");
+            term = scanner.nextLine();
+            terms.add(term);
+            System.out.println("The definition for card #" + i + ":");
+            definition = scanner.nextLine();
+            definitions.add(definition);
+        }
+        for(int i = 0; i < terms.size(); i++) {
+            System.out.println("Print the definition of " + "\"" + terms.get(i) + "\":");
+            String answer = scanner.nextLine();
+            if (answer.equals(definitions.get(i))) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Wrong. The right answer is \"" + definitions.get(i) + "\"");
+            }
         }
     }
 }
